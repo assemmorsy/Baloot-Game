212ce737c73a65ac8bd01b2399b04e37f528d7a4
Mohamed Yahia <m.yahia.eid2011@gmail.com>
2023-04-07 02:32:30 +0200

    fix match lifecycle

--- src/api/match/content-types/match/lifecycles.js
+++ src/api/match/content-types/match/lifecycles.js
@@ -3,21 +3,35 @@ const { ApplicationError } = require("@strapi/utils").errors;
 module.exports = {
 
     beforeCreate(event) {
-        let data = event.params.data;
-        if (data.team_1 || data.team_2 || data.team_1 === data.team_2) {
 
-            throw new ApplicationError("consider it a UI Error :D")
+        let data = event.params.data;
+
+        if (data.team_1.connect.length == 0 || data.team_2.connect.length == 0) {
+
+            throw new ApplicationError("You Must Enter Two Teams")
+        }
+        if (data.team_1.connect[0].id == data.team_2.connect[0].id) {
+
+            throw new ApplicationError("The Two Teams Must Be Different")
         }
     },
 
     beforeUpdate(event) {
         let data = event.params.data;
-        if (data.team_1 || data.team_2 || data.team_1 === data.team_2) {
 
-            throw new ApplicationError("consider it a UI Error :D")
+        // console.log("team1")
+        // console.log(data.team_1)
+        // console.log("team2")
+        // console.log(data.team_2)
+        if (data.team_1.connect.length == 0 || data.team_2.connect.length == 0) {
+            throw new ApplicationError("You Must Enter Two Teams")
+            
         }
-
-        // this part is being handled in the amin panel 
+        if (data.team_1.connect[0].id == data.team_2.connect[0].id) {
+           
+            throw new ApplicationError("The Two Teams Must Be Different")
+        }
+        // this part is being handled in the admin panel 
         // using the deafult value = 0
 
         // let data = event.params.data;
