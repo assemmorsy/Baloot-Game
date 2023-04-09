e112070c7d4e1b090a7a764564aed78453f42e1a
Assem Morsy <assemmorsy@outlook.com>
2023-04-09 17:08:22 +0200

    update

--- src/api/match/content-types/match/lifecycles.js
+++ src/api/match/content-types/match/lifecycles.js
@@ -15,14 +15,6 @@ module.exports = {
         }
     },
 
-<<<<<<< HEAD
-    beforeUpdate(event) {
-        // let data = event.params.data;
-        // if (data.team_1 || data.team_2 || data.team_1 === data.team_2) {
-
-        //     throw new ApplicationError("consider it a UI Error :D")
-        // }
-=======
     async beforeUpdate(event) {
         let params = event.params;
         let matchId = params.where.id;
@@ -45,7 +37,6 @@ module.exports = {
         if (params.data.team_2.disconnect.length !== 0 && params.data.team_2.connect.length == 0) {
             throw new ApplicationError("you must choose team 1")
         }
->>>>>>> 27d6e6ef5310be05d36e0926859a4bb9aee0208f
 
         // the user updated team 1 with same value as team 2
         if (params.data.team_1.connect.length !== 0) {
