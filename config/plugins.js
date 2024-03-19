1191d0f4f6de6625aeb4a457e81485021822522c
assem morsy <assemmorsy@outlook.com>
2024-03-19 23:34:53 +0200

    solve jwt secret issue

--- config/plugins.js
+++ config/plugins.js
@@ -5,10 +5,5 @@ module.exports = ({ env }) => ({
             sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
         }
     },
-    //! TODO  remove the limit
-    'users-permissions' : {
-        config:{
-            ratelimit:{ interval: 60000, max: 100000 }
-        }
-    }
+    
 });
